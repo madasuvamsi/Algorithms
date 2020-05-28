7e86c568c25ca273e31e0b90caf574483f7c5736
madasuvamsi <madasuvamsi@gmail.com>
2020-05-27 18:00:07 -0700

    Added Duplicate Check Algorithm

---
+++ Algorithms/DuplicateCheck/DupCheckMethodTwo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateCheck
+{
+    class DupCheckMethodTwo
+    {
+        public static void Main(string[] args)
+        {
+            int[] sampleArray = new int[4] { 1, 2, 3, 4 };
+
+            bool result = DupCheck(sampleArray);
+            if (result == true)
+            {
+                Console.WriteLine("The array contains duplicate elements");
+            }
+            else
+            {
+                Console.WriteLine("There are no duplciate elemts in the array");
+            }
+        }
+
+
+        /*
+         Array.Sort() method uses quick sort
+         On average, this method is an O(n log n) operation, where n is Count;
+         in the worst case it is an O(n2) operation.
+
+         The Time complexity for this alforithm is O(nlogn) for Average case
+         and O(n2) for Worst case and the Space complexity is O(1)
+             */
+
+        public static bool DupCheck(int[] arr)
+        {
+            Array.Sort(arr);
+
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if(arr[i]==arr[i+1])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Algorithms/DuplicateCheck/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input 1
+            //int[] sampleArray = new int[4] { 1, 2, 3, 1 };
+
+            //input 2
+            int[] sampleArrayTwo = new int[4] { 1, 2, 3, 4 };
+
+            bool result = ContainsDups(sampleArrayTwo);
+
+            if(result==true)
+            {
+                Console.WriteLine("The array contains duplicate elements");
+            }
+            else
+            {
+                Console.WriteLine("There are no duplciate elemts in the array");
+            }
+        }
+
+        /*Time Complexity for this will be O(n) and 
+         * space complexity is O(n) as we are using a hashset*/
+        public static bool ContainsDups(int[] arr)
+        {
+            /*
+             The HashSet<T> class provides high-performance set operations. 
+             A set is a collection that contains no duplicate elements,
+             and whose elements are in no particular order.
+             */
+
+            HashSet<int> numbers = new HashSet<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(numbers.Contains(arr[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    //Add integers to mumbers hashset if they are not already present in the hashset
+                    numbers.Add(arr[i]);
+                }
+            }
+
+            //If no dups are present return false
+            return false;
+        }
+    }
+}
